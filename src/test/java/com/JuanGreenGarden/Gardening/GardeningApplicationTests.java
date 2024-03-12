1aa4f636474e85cefde561f20bc816c2138b91e4
DavidSantos001 <ds0299646@gmail.com>
2024-03-12 13:04:35 -0500

    feat: :tada: Project structure
    folders were created for the project structure

---
+++ src/test/java/com/JuanGreenGarden/Gardening/GardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuanGreenGarden.Gardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
