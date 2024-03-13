feea5ea76ffec88bfbb6a30d18ade93962c5e385
AngieNC <139162495+AngieNC@users.noreply.github.com>
2024-03-12 21:54:32 -0500

    feat: :sparkles: Add security to the project
    Add security files tothe project with the objective to lock the public users

---
+++ src/main/java/com/JuanGreenGarden/Gardening/web/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.JuanGreenGarden.Gardening.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JuanGreenGarden.Gardening.domain.security.JWTAuthorizationConfig;
+import com.JuanGreenGarden.Gardening.persistence.entity.UserProject;
+
+@RestController
+public class LoginController {
+    
+    @Autowired
+    JWTAuthorizationConfig jwtAuthtenticationConfig;
+
+    @PostMapping("login")
+    public UserProject login(
+            @RequestParam("user") String username,
+            @RequestParam("encryptedPass") String encryptedPass) {
+                
+
+
+        String token = jwtAuthtenticationConfig.getJWTToken(username);
+        return new UserProject(username, encryptedPass, token);
+    }
+}
+
