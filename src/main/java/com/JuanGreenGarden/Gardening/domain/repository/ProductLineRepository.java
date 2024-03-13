9819f40489789f21058feaebf9ca8971dfa4d3bf
AngieNC <139162495+AngieNC@users.noreply.github.com>
2024-03-13 12:29:45 -0500

    feat: :sparkles: Add repository files
    Almost repository files were created

---
+++ src/main/java/com/JuanGreenGarden/Gardening/domain/repository/ProductLineRepository.java
@@ -0,0 +1,11 @@
+package com.JuanGreenGarden.Gardening.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.JuanGreenGarden.Gardening.persistence.entity.ProductLine;
+
+@Repository
+public interface ProductLineRepository extends JpaRepository<ProductLine, String>{
+    
+}
