af6d787c5112f6f3b1f84f53081a6129bf893fc4
robersks <robersks.s@gmail.com>
2024-03-15 18:11:38 -0500

    feat: :sparkles: add DTO Customer

---
+++ src/main/java/com/JuanGreenGarden/Gardening/persistence/entity/DTO/CustomerDTO.java
@@ -0,0 +1,123 @@
+package com.JuanGreenGarden.Gardening.persistence.entity.DTO;
+
+public class CustomerDTO {
+    private Integer customerNumber;
+    private String customerName;
+    private String contactFirstName;
+    private String contactLastName;
+    private String phone;
+    private String fax;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String region;
+    private String country;
+    private String postalCode;
+    private double creditLimit;
+
+    public Integer getCustomerNumber() {
+        return this.customerNumber;
+    }
+
+    public void setCustomerNumber(Integer customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactFirstName() {
+        return this.contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getContactLastName() {
+        return this.contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return this.fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getAddressLine1() {
+        return this.addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return this.addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return this.region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public double getCreditLimit() {
+        return this.creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    
+}
